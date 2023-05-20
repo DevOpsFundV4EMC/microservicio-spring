b835ae2f9b589bc3f316c9629ba7a953b4188eaa
elmo1963 <57698672+elmo1963@users.noreply.github.com>
2023-05-20 16:50:19 -0400

    Update WebLayerTest.java

--- src/test/java/com/example/testingweb/WebLayerTest.java
+++ src/test/java/com/example/testingweb/WebLayerTest.java
@@ -22,7 +22,7 @@ public class WebLayerTest {
 	@Test
 	public void shouldReturnDefaultMessage() throws Exception {
 		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-				.andExpect(content().string(containsString("Hello, World")));
+				.andExpect(content().string(containsString("Finaliza el curso DevOps Foundation V4 - Enrique Moraga")));
 	}
 }
 //end::test[]
